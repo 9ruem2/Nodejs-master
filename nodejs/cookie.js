e43f2133db39b760265ff73336f663737b636d2e
9ruem2 <9ruem2@naver.com>
2024-02-07 16:02:56 +0900

    session VS permanent

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -14,7 +14,10 @@ http.createServer(function(request, response){
 
     // 쿠키 생성
     response.writeHead(200, {
-        'Set-Cookie' : ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+        'Set-Cookie' : [
+            'yummy_cookie=choco', 
+            'tasty_cookie=strawberry',
+            `Permanent=cookies; Max-Age = ${60*60*24*30}`]
     });
     response.end('Cookie!!');
 
